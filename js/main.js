68400a3852ea063d3c0f745969ed8f92858f114e
Yibin Cui <110690068+Accusamus2065@users.noreply.github.com>
2025-03-06 12:26:02 -0500

    added data for visualization

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-import { ScrollManager } from './scroll.js';
-import { initPage1 } from '../../../project/CSC316-project/js/page1.js';
-import { initPage2 } from '../../../project/CSC316-project/js/page2.js';
-import { initPage3 } from "../../../project/CSC316-project/js/page3.js";
+import {ScrollManager} from './scroll.js';
+import {initPage1} from './page1';
+import {initPage2} from './page2';
+import {initPage3} from './page3';
 
 // Initialize scroll manager
 const scrollManager = new ScrollManager();
