0d3f378c41c9aa26669ceae1e23a6c41a1e26db4
Gilbert Ye <gilbert.ye@mail.utoronto.ca>
2025-03-06 15:51:22 -0500

    added styles to the page and added some story elements

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
 import { ScrollManager } from './scroll.js';
-import { initPage1 } from '../../../project/CSC316-project/js/page1.js';
-import { initPage2 } from '../../../project/CSC316-project/js/page2.js';
-import { initPage3 } from "../../../project/CSC316-project/js/page3.js";
+import { initPage0 } from './page0.js';
+import { initPage1 } from './page1.js';
+import { initPage2 } from './page2.js';
+import { initPage3 } from './page3.js';
+import { initPage4 } from './page4.js';
+import { initPage5 } from './page5.js';
 
 // Initialize scroll manager
 const scrollManager = new ScrollManager();
+window.scrollManager = scrollManager;
 
-// Initialize page-specific code
+// Initialize all page-specific code when the DOM is ready
 document.addEventListener('DOMContentLoaded', () => {
+    initPage0();
     initPage1();
     initPage2();
     initPage3();
+    initPage4();
+    initPage5();
 });
