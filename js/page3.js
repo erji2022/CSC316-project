a4219a5b844d8ba009388078fe92ba6616ea6f3a
Yibin Cui <110690068+Accusamus2065@users.noreply.github.com>
2025-03-06 17:57:52 -0500

    finished vis5

--- js/page3.js
+++ js/page3.js
@@ -6,7 +6,6 @@ export function initPage3() {
 
     function initializeVisualizations() {
         // Your vis5 and vis6 initialization code
-        // d3.select('#vis5').html('Vis 5 Initialized');
         renderClimateChart();
         d3.select('#vis6').html('Vis 6 Initialized');
     }
